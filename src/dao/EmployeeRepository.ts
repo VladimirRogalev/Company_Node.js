81a7042a8e457a1016c6405de174b2b0f0189dec
Vladimir <vladimirrogalev1@gmail.com>
2024-12-21 15:22:44 +0200

    method implementation changed

--- src/dao/EmployeeRepository.ts
+++ src/dao/EmployeeRepository.ts
@@ -1,48 +1,48 @@
-import * as fs from "node:fs";
-import {NoParamCallback, ObjectEncodingOptions, PathOrFileDescriptor, WriteFileOptions} from "node:fs";
-import {Abortable} from "node:events";
+import * as fs from 'node:fs';
 import Employee from '../models/Employee';
 
-const EMPLOYEES_DATA_FILE= "db.txt";
 
 export default class EmployeeRepository {
-    private fsReadable:{
-        (path: PathOrFileDescriptor, options: (({
-            encoding?: null | undefined;
-            flag?: string | undefined
-        } & Abortable) | undefined | null), callback: (err: (NodeJS.ErrnoException | null), data: Buffer) => void): void;
-        (path: PathOrFileDescriptor, options: (({
-            encoding: BufferEncoding;
-            flag?: string | undefined
-        } & Abortable) | BufferEncoding), callback: (err: (NodeJS.ErrnoException | null), data: string) => void): void;
-        (path: PathOrFileDescriptor, options: ((ObjectEncodingOptions & {
-            flag?: string | undefined
-        } & Abortable) | BufferEncoding | undefined | null), callback: (err: (NodeJS.ErrnoException | null), data: (string | Buffer)) => void): void;
-        (path: PathOrFileDescriptor, callback: (err: (NodeJS.ErrnoException | null), data: Buffer) => void): void
-    };
-    private fsWritable:{
-        (file: PathOrFileDescriptor, data: (string | NodeJS.ArrayBufferView), options: WriteFileOptions, callback: NoParamCallback): void;
-        (path: PathOrFileDescriptor, data: (string | NodeJS.ArrayBufferView), callback: NoParamCallback): void
-    };
+    private readonly filePath: string;
 
-    constructor() {
-        this.fsReadable = fs.readFile;
-        this.fsWritable = fs.writeFile;
-    }
-
-    // private path = "db.txt";
-
-
-    read (callback :(err: Error| null, data: string) => void ):void {
-    this.fsReadable(EMPLOYEES_DATA_FILE,'utf-8',callback);
+    constructor(filePath: string = './db.txt') {
+        this.filePath = filePath;
     }
 
 
-    write (employee: Employee) {
-       this.fsWritable(this.path, JSON.stringify(employee)+'\n', () =>{})
+    readAll(): Employee[] {
+        try {
+            const res = fs.readFileSync(this.filePath, 'utf-8');
+            return JSON.parse(res) as Employee[];
+        } catch (err: any) {
+            console.error(`Error -> ${err}`);
+            return [];
+        }
+    }
+
+
+    write(employee: Employee) {
+        try {
+            const employees = this.readAll();
+            employees.push(employee);
+            this.writeAll(employees);
+            return true;
+        } catch (err: any) {
+            return false;
+        }
     };
 
-    writeAll (... employees: Employee[]) {
-        this.fsWritable(EMPLOYEES_DATA_FILE, JSON.stringify(employees, null, 2), 'utf-8',()=>{})
+    writeAll(employees: Employee[]) {
+        try {
+            console.log(employees)
+            const data = JSON.stringify(employees, null,2);
+            fs.writeFileSync(this.filePath, data, 'utf-8');
+            console.log('Success');
+            return true;
+        } catch (err: any) {
+            console.log(err);
+            return false;
+
+        }
     }
 }
--- src/services/EmployeeServiceImpl.ts
+++ src/services/EmployeeServiceImpl.ts
@@ -1,43 +1,34 @@
 import {EmployeeService} from './EmployeeService';
-import Employee from "../models/Employee";
+import Employee from '../models/Employee';
 import EmployeeRepository from '../dao/EmployeeRepository';
-import {eventEmitter} from '../events/eventEmitter';
 
 export class EmployeeServiceImpl implements EmployeeService {
 
     private employeeRepository: EmployeeRepository = new EmployeeRepository();
     private employees: Employee[] = [];
-    constructor() {
-        this.employeeRepository.read((err, data) => {
-            if(err) {
-                eventEmitter.emit("error", err)
-            } else {
-                this. employees = JSON.parse(data);
-            }
-        })
-    }
+
 
     getAllEmployees(): Employee[] {
-        return this.employees;
+        return this.employeeRepository.readAll();
     }
 
     addEmployee(employee: Employee): boolean {
-        if(this.employees.findIndex(elem => elem.id === employee.id) === -1){
-            this.employees.push(employee);
-            this.employeeRepository.writeAll(...this.employees);
+        if (this.getAllEmployees().findIndex(elem => elem.id === employee.id) === -1) {
+            this.employeeRepository.write(employee);
             return true;
         }
         return false;
     }
 
     deleteEmployee(id: number): Employee | null {
-        const index = this.employees.findIndex(elem => elem.id === id);
-        const victim = this.employees.at(index) as Employee;
-        if(index === -1) {
+        let allEmployees = this.getAllEmployees();
+        const index = this.getAllEmployees().findIndex(elem => elem.id === id);
+        const victim = this.getAllEmployees().at(index) as Employee;
+        if (index === -1) {
             return null;
         }
-        this.employees.splice(index, 1)
-        this.employeeRepository.writeAll(...this.employees);
+        allEmployees.splice(index, 1);
+        this.employeeRepository.writeAll(allEmployees);
         return victim;
     }
 }
